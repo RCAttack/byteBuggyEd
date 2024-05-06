f4ffec2da794a4fde4e7efa33a1715b27f81a144
EddieBrito <hbrito@leomail.tamuc.edu>
2024-05-06 05:41:49 -0500

    save

--- ByteBuggy.py
+++ ByteBuggy.py
@@ -3,5 +3,5 @@
 # Note: This script runs ByteBuggy from within a cloned git repo.
 # The script `bin/ByteBuggy` is designed to be run after installing (from /usr/sbin), not from the cwd.
 
-from ByteBuggy import __main__
+from byteBuggy import __main__
 __main__.entry_point()
